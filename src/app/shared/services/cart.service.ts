5b993f83fc0d2c63edfcae3085e66df3ef31b0b8
AhmedBahgatGad <firlongad@gmail.com>
2024-03-13 16:15:18 +0200

    edit

--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -35,7 +35,7 @@ export class CartService {
     })
   }
   checkOut(cartId:string, userData:object):Observable<any>{
-    return this._HttpClient.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=https://github.com/AhmedBahgatGad/ecommerce.git`,
+    return this._HttpClient.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=https://github.com/AhmedBahgatGad/ECommerce.git`,
     {
       shippingAddress: userData
     })
